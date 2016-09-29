f589f70f459cd1b012df22a0d06d6bbd0dc14e0f
tommy3ua <tommy3@ualberta.ca>
2016-09-29 17:18:43 -0600

    lab4

---
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -0,0 +1,18 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by tommy3 on 9/29/16.
+ */
+public class TweetListTest extends ActivityInstrumentationTestCase2 {
+
+    public TweetListTest() {
+        super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
+    }
+
+    // test methods always start with test
+    public void testHasTweet() {
+        assertTrue(Boolean.FALSE);  // fails
+    }
+}
